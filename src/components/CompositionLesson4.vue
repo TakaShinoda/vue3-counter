90f78f14946b4a1af6f863eb36803efdb91292df
Takahiro <tkhr_621@docomo.ne.jp>
2021-09-04 13:22:43 +0900

    watch

--- src/components/CompositionLesson4.vue
+++ src/components/CompositionLesson4.vue
@@ -1,12 +1,13 @@
 <template>
     <!-- watchEffect: 引数で受け取った関数内のリアクティブな値に変化があった際に、その関数を都度実行 -->
+    <!-- computedとは異なりrefオブジェクトを返さないが代わりに監視を停止させるための関数を返す -->
     <h1>composition API 色々試す4</h1>
     <button @click="increment">increment</button>
     <button @click="stopHandler">stop</button>
 </template>
 
 <script>
-import { ref, watchEffect } from 'vue'
+import { ref, watch, watchEffect } from 'vue'
 
 export default {
     setup() {
@@ -19,6 +20,16 @@ export default {
         // increment関数が動作するたび現在のカウントが出力される
         // 監視をストップさせるための関数が返される
         const stopHandler = watchEffect(() => console.log(count.value))
+
+        // watchはoptions APIと同じ
+        const state = ref('0')
+
+        // 監視する値と変更があった際に実行する関数を受け取る
+        watch(state, (next, prev) => {
+            if(typeof prev === 'string' && typeof next === 'number') {
+                console.log('move string => number')
+            }
+        })
 
         return {
             increment,
